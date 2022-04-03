81088dc623d22c593caa0f6846e92e38dc4b61f6
NIR <MAILMAIL10@GMAIL.COM>
2022-04-03 20:59:17 +0300

    project initialize

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,6 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () =>
       import('./login/login.module').then((m) => m.LoginModule),
-      // canActivate: [AuthGuard],
   },
   {
     path: 'products',
